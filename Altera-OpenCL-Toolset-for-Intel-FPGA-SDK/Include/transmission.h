13ae813044440694ddd667dfaaa7d5f3c6a72f47
Zygal <jnovor@comcast.net>
2018-03-15 14:05:59 -0400

    Delete transmission.h

--- Altera-OpenCL-Toolset-for-Intel-FPGA-SDK/Include/transmission.h
+++
@@ -1,9 +0,0 @@
-#ifndef TRANSMISSION_H_   /* Include guard */
-#define TRANSMISSION_H_
-
-/*function prototypes*/
-
-///@brief This main function is the system control of the application
-void Send(void);
-
-#endif // TRANSMISSION_H_
