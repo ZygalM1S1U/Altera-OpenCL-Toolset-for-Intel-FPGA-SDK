99e0e6562018e480578e039e7d7fd0f34b6d64a2
Zygal <jnovor@comcast.net>
2018-03-15 09:40:58 -0400

    Delete boardConfig.h

--- boardConfig.h
+++
@@ -1,9 +0,0 @@
-#ifndef BOARDCONFIG_H_   /* Include guard */
-#define BOARDCONFIG_H_
-
-/*function prototypes*/
-
-///@brief This main function is the system control of the application
-void BoardConfig(void);
-
-#endif // BOARDCONFIG_H_
